c7aa50e1d80bec2f333aae1a25136041d2110f7f
magicmarkh <mark.t.hurter@gmail.com>
2025-07-22 16:24:44 -0500

    comment

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -52,7 +52,7 @@ module "ec2_public_server" {
   linux_security_group_ids   = module.security_groups.trusted_ssh_external_security_group_id
   windows_security_group_ids = module.security_groups.trusted_rdp_external_security_group_id
 }
-*/
+/*
 module "jenkins_server_role" {
   source           = "./modules/security/iam_roles/jenkins_server_role"
   team_name        = var.team_name
