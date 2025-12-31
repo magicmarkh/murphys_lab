10676711e5ba256fbc4095ba7de417c7cc0a6176
magicmarkh <mark.t.hurter@gmail.com>
2025-12-31 10:55:08 -0600

    naming & organization changes

--- terraform_code/03_cyberark/main.tf
+++ terraform_code/03_cyberark/main.tf
@@ -8,7 +8,7 @@ data "terraform_remote_state" "foundation" {
 
   config = {
     bucket = "us-ent-east"
-    key    = "terraform/foundation.tfstate"
+    key    = "terraform/01_foundation.tfstate"
     region = "us-east-2"
   }
 }
@@ -21,7 +21,7 @@ data "terraform_remote_state" "security" {
 
   config = {
     bucket = "us-ent-east"
-    key    = "terraform/security.tfstate"
+    key    = "terraform/02_security.tfstate"
     region = "us-east-2"
   }
 }
--- terraform_code/05_databases/main.tf
+++ terraform_code/05_databases/main.tf
@@ -8,7 +8,7 @@ data "terraform_remote_state" "foundation" {
 
   config = {
     bucket = "us-ent-east"
-    key    = "terraform/foundation.tfstate"
+    key    = "terraform/01_foundation.tfstate"
     region = "us-east-2"
   }
 }
