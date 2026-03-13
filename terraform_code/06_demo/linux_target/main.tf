04a658981a9a0a659782190012d600254886ab4e
magicmarkh <mark.t.hurter@gmail.com>
2026-03-13 13:59:55 -0500

    updated key path

--- terraform_code/06_demo/linux_target/main.tf
+++ terraform_code/06_demo/linux_target/main.tf
@@ -134,7 +134,7 @@ resource "aws_instance" "demo_linux_target" {
 resource "idsec_sia_ssh_public_key" "demo_linux_target_key" {
   target_machine = aws_instance.demo_linux_target.private_ip
   username = "ec2-user"
-  private_key_path = "../keys/demo_linux_target_key.pem"
+  private_key_path = "../../03_ec2_compute/key_pair/us-ent-east-key.pem"
 
   depends_on = [aws_instance.demo_linux_target]
 
