7eabb2f36b0dedbf837b7c7e8559bf53303b8a28
magicmarkh <mark.t.hurter@gmail.com>
2026-03-13 13:50:41 -0500

    update ouptuts

--- terraform_code/06_demo/linux_target/outputs.tf
+++ terraform_code/06_demo/linux_target/outputs.tf
@@ -11,20 +11,4 @@ output "instance_private_ip" {
   value       = aws_instance.demo_linux_target.private_ip
 }
 
-output "instance_ami" {
-  description = "AMI ID used for the instance"
-  value       = aws_instance.demo_linux_target.ami
-}
 
-# ===========================
-# SSH Public Key Outputs
-# ===========================
-output "ssh_public_key_id" {
-  description = "ID of the CyberArk-managed SSH public key"
-  value       = idsec_sia_ssh_public_key.demo_linux_target_key.public_key_id
-}
-
-output "ssh_key_fingerprint" {
-  description = "Fingerprint of the SSH public key"
-  value       = idsec_sia_ssh_public_key.demo_linux_target_key.fingerprint
-}
