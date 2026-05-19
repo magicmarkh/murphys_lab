528ab09a476392e561044c9bd9cb8070e0922527
magicmarkh <mark.t.hurter@gmail.com>
2026-05-19 16:20:42 -0500

    ignore changes on key file

--- terraform_code/03_ec2_compute/key_pair/main.tf
+++ terraform_code/03_ec2_compute/key_pair/main.tf
@@ -1,6 +1,10 @@
 resource "tls_private_key" "server" {
   algorithm = "RSA"
   rsa_bits  = 4096
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "aws_key_pair" "server" {
@@ -10,6 +14,10 @@ resource "aws_key_pair" "server" {
   tags = {
     Name  = "${var.team_name}-key"
     Owner = var.asset_owner_name
+  }
+
+  lifecycle {
+    ignore_changes = all
   }
 }
 
